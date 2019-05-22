45c27f82c83f6e0923ce54b4ec4c52085a3f36ca
karamalesa1980 <karamalesa@gmail.com>
2019-05-22 21:07:39 +0300

    Chenge/contects/new-> 'contacts'

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'home/index'
-  root 'home#error404'
-  resource :contacts, only: [:new, :create]
+  
+  resource :contacts, only: [:new, :create], path_names: {:new => ''}
   resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

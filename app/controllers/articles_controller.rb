b9f0cc60af6bfd99393967914255f287022ccfea
karamalesa1980 <karamalesa@gmail.com>
2019-05-25 22:37:29 +0300

    Lesson 39.1 finished

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,9 +6,14 @@ class ArticlesController < ApplicationController
      @article = Article.new(article_params)
     if @article.valid?
       @article.save
+      redirect_to @article
     else
       render action: 'new'
     end   
+  end
+
+  def show
+     @article = Article.find(params[:id]) 
   end  
 
 private

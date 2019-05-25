b570d9b32bd50b23223d57927726ec393c689784
karamalesa1980 <karamalesa@gmail.com>
2019-05-25 21:54:56 +0300

    Add validates to Articles

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'home/index'
   
   resource :contacts, only: [:new, :create], path_names: {:new => ''}
-  resources :articles
+  resources :articles, path_names: {:new => ''}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

d74a17a10fc4ebb6923376432aa5e23a6da740be
karamalesa1980 <karamalesa@gmail.com>
2019-05-29 23:31:14 +0300

    add action articles update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,8 +15,7 @@ class ArticlesController < ApplicationController
 
   def create
      @article = Article.new(article_params)
-    if @article.valid?
-      @article.save
+    if @article.save
       redirect_to @article
     else
       render action: 'new'
@@ -28,7 +27,12 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    
+    @article = Article.find(params[:id])
+     if @article.update(article_params)
+      redirect_to @article
+    else
+      render action: 'edit'
+    end
   end
 
   

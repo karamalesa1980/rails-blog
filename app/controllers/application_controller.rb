198bf246e152953b3f50a5ad39bb8f2fb102fd86
karamalesa1980 <karamalesa@gmail.com>
2019-07-04 22:35:02 +0300

    permit username for sing_up form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!, except: [:show, :index]
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
   
 
   def after_sign_out_path_for(resource_or_scope)

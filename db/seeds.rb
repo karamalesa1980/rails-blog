8599e5028adf15f5ec81ff5ada8a264bd4c4fc18
karamalesa1980 <karamalesa@gmail.com>
2019-07-13 21:03:27 +0300

    add admin to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+admin_model     = RailsAdmin::AbstractModel.new(Admin)
+admin_model.new(:email => 'karamalesa@gmail.com', :password => 'qwerty', :password_confirmation => 'qwerty').save

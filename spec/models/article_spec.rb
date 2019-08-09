25f12035b9110dd9e601234f6d2843b13d1ed5f7
karamalesa1980 <karamalesa@gmail.com>
2019-08-10 00:25:26 +0300

    lesson 46 (41 minutes)

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  it { should have_many(:comments).dependent(:destroy) }
+  describe "associations" do
+    it { should have_many(:comments).dependent(:destroy) }
+  end 
+  describe "validations" do 
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:text) }
+  end
 end

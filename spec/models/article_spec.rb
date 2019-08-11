f20ca460550343510b2b68c72383215c7fd52bb7
karamalesa1980 <karamalesa@gmail.com>
2019-08-11 11:44:48 +0300

    add test #subject

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -8,4 +8,7 @@ RSpec.describe Article, type: :model do
     it { should validate_presence_of(:title) }
     it { should validate_presence_of(:text) }
   end
+  describe "#subject" do
+    it "returns the article title"
+  end  
 end

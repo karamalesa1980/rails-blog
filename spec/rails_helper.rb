8f44ef1bc43efd568c4e5babbcf617a2ef7eee36
karamalesa1980 <karamalesa@gmail.com>
2019-08-11 19:43:20 +0300

    add factory_bot to RSpec configure

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -60,7 +60,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-  config.include FactoryGirl::Syntax::Methods
+  config.include FactoryBot::Syntax::Methods
 end
 
 Shoulda::Matchers.configure do |config|

c0babd0f1d5a5f2d3e31fc41baf291bf7943d9b9
karamalesa1980 <karamalesa@gmail.com>
2019-08-22 23:38:22 +0300

    add validates length title and text

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
-  validates :title, presence: true
-  validates :text, presence: true
+  validates :title, presence: true, length: { maximum: 140 }
+  validates :text, presence: true, length: { maximum: 4000 }
 
   mount_uploader :picture, PictureUploader 
 

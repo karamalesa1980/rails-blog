644490c906b910aa4c9caee8e594eff66ccba9ee
karamalesa1980 <karamalesa@gmail.com>
2019-08-22 23:40:28 +0300

    add test length title and text

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe Article, type: :model do
     it { should have_many(:comments).dependent(:destroy) }
   end 
   describe "validations" do 
-    it { should validate_presence_of(:title) }
-    it { should validate_presence_of(:text) }
+    it { should validate_length_of(:title).is_at_most(140) }
+    it { should validate_length_of(:text).is_at_most(4000) }
   end
   describe "#subject" do
     it "returns the article title" do

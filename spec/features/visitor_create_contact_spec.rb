cf7a9d3beb9c0937efec775cb79f6c410454cb42
karamalesa1980 <karamalesa@gmail.com>
2019-08-25 23:52:52 +0300

    add create contacts test capibara

---
+++ spec/features/visitor_create_contact_spec.rb
@@ -0,0 +1,11 @@
+require "rails_helper"
+
+
+feature "Contact creation" do
+    scenario "allows acess to contacts page" do
+      visit new_contacts_path
+      
+  
+      expect(page).to have_content "Contact us"
+    end
+end

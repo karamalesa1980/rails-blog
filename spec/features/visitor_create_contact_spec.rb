cbf91f0ec61f88b3d61c66ec32db24c19728f6dd
karamalesa1980 <karamalesa@gmail.com>
2019-09-09 23:22:26 +0300

    lesson  47 1 hour 10 minutes

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -2,10 +2,33 @@ require "rails_helper"
 
 
 feature "Contact creation" do
-    scenario "allows acess to contacts page" do
+
+    scenario "allows access to contacts page" do
       visit new_contacts_path
       
   
       expect(page).to have_content  I18n.t 'contacts.contact_us'
+    end
+
+    scenario "allows a guest to create contact" do
+
+
+      visit new_user_session_path
+
+      fill_in :user_email, with: 'polo@gmail.com'
+      fill_in :user_password, with: 'qwerty'
+
+      click_button 'Log in'
+      expect(page).to have_content 'Log in :)'
+
+      
+
+      visit new_contacts_path
+      fill_in :contact_email, with: 'admin@gmail.com'
+      fill_in :contact_message, with: 'admin@gmail.com'
+  
+      click_button 'Отправить сообщение'
+      
+      expect(page).to have_content 'Log in :)'
     end
 end
---
+++ spec/features/visitor_creates_contact_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+feature "Account Creation" do
+  scenario "allows guest to create account" do
+    visit new_user_registration_path
+
+    fill_in :user_email, with: 'popo@gmail.com'
+    fill_in :user_username, with: 'Popo'
+    fill_in :user_password, with: 'qwerty'
+    fill_in :user_password_confirmation, with: 'qwerty'
+
+    click_button 'Sign up'
+    expect(page).to have_content I18n.t('devise.registrations.signed_up')
+
+    visit new_contacts_path
+    fill_in :contact_email, with: 'admin@gmail.com'
+    fill_in :contact_message, with: 'admin@gmail.com'
+
+    click_button 'Отправить сообщение'
+    
+    expect(page).to have_content 'Thank you'
+  end
+  
+end

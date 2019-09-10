a763d7f5ff5215396b12c66620075d70f953a256
karamalesa1980 <karamalesa@gmail.com>
2019-09-10 23:28:15 +0300

    before (:all not working) (:each working)

--- spec/features/user_creates_article_spec.rb
+++ spec/features/user_creates_article_spec.rb
@@ -1,9 +1,12 @@
 require "rails_helper"
-require "support/session_helper"
+
 
 feature "Article Creation" do
-  scenario "allows visit user to new article page" do
+  before(:each) do
     sign_up
+  end  
+  scenario "allows visit user to new article page" do
+    
     expect(page).to have_content I18n.t('devise.registrations.signed_up')
 
     visit new_article_path

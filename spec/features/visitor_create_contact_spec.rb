3509af82792c0486fffff91b375305315fc1f75d
karamalesa1980 <karamalesa@gmail.com>
2019-09-12 17:59:30 +0300

    lesson 47 dz finished

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -3,28 +3,23 @@ require "rails_helper"
 
 
 feature "Contact creation" do
-
+  before(:each) do
+    sign_up
+  end
     scenario "allows access to contacts page" do
       visit new_contacts_path
       
-  
       expect(page).to have_content  I18n.t('contacts.contact_us')
     end
 
     scenario "allows a guest to create contact" do
 
-      sign_up
-     
-      expect(page).to have_content I18n.t('devise.registrations.signed_up')
-
-      
-
       visit new_contacts_path
+      
       fill_in :contact_email, with: 'admin@gmail.com'
       fill_in :contact_message, with: 'admin@gmail.com'
   
-      click_button 'Отправить сообщение'
-      
+      click_button('Отправить сообщение')
       expect(page).to have_content 'Thank you'
     end
 end
--- spec/features/visitor_creates_contact_spec.rb
+++ spec/features/visitor_creates_contact_spec.rb
@@ -2,16 +2,17 @@ require "rails_helper"
 
 
 feature "Account Creation" do
-  scenario "allows guest to create account" do
+  before(:each) do
     sign_up
-    expect(page).to have_content I18n.t('devise.registrations.signed_up')
-
+  end  
+  scenario "allows guest to create account" do
+    
     visit new_contacts_path
+
     fill_in :contact_email, with: 'admin@gmail.com'
     fill_in :contact_message, with: 'admin@gmail.com'
 
-    click_button 'Отправить сообщение'
-    
+    click_button('Отправить сообщение')
     expect(page).to have_content 'Thank you'
   end
   

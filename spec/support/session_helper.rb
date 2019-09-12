508d89ddac19333e84daacb00bb33d0d301dd389
karamalesa1980 <karamalesa@gmail.com>
2019-09-12 17:57:08 +0300

    add def (create_new_article) to session_helper

--- spec/support/session_helper.rb
+++ spec/support/session_helper.rb
@@ -6,5 +6,15 @@ def sign_up
   fill_in :user_password, with: 'qwerty'
   fill_in :user_password_confirmation, with: 'qwerty'
 
-  click_button 'Sign up'
+  click_button('Sign up')
+  expect(page).to have_content I18n.t('devise.registrations.signed_up')
+end  
+
+def create_new_article
+  visit new_article_path
+
+  fill_in :article_title, with: 'Hello!'
+  fill_in :article_text, with: 'Hello user'
+
+  click_button('Опубликовать')
 end  

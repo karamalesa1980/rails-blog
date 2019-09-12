0776c88cb460b4f82e2c310767f191779a4b8b1c
karamalesa1980 <karamalesa@gmail.com>
2019-09-12 17:59:03 +0300

    add test edit_article

--- spec/features/user_creates_article_spec.rb
+++ spec/features/user_creates_article_spec.rb
@@ -4,15 +4,25 @@ require "rails_helper"
 feature "Article Creation" do
   before(:each) do
     sign_up
+    create_new_article
   end  
   scenario "allows visit user to new article page" do
     
-    expect(page).to have_content I18n.t('devise.registrations.signed_up')
-
     visit new_article_path
     
     
     expect(page).to have_content I18n.t('article.new_article')
+  end
+
+  scenario "allows user to edit article" do
+    
+    click_link('Редактировать')
+    expect(page).to have_content 'Edit Article'
+    fill_in :article_title, with: 'Hello!!!'
+    fill_in :article_text, with: 'Hello user'
+
+    click_button('Сохранить')
+    expect(page).to have_content 'Hello!!!'
   end
   
 end 
